ed2ed53ad5e6413d0d023f0de96106e0126655da
rohith <rohithaug@gmail.com>
2023-11-26 16:46:09 -0800

    make username unique

--- server/backend/src/app/models/user.model.js
+++ server/backend/src/app/models/user.model.js
@@ -11,7 +11,8 @@ const userSchema = mongoose.Schema(
     {
         name: {
             type: String,
-            required: true
+            required: true,
+            unique: true,
         },
         email: {
             type: String,
